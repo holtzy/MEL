13f8ef490b164e8023d0cc37db24e73d36b9cd7a
Holtz Yan <yan.holtz@datadoghq.com>
2025-04-07 09:51:47 +0200

    k

--- src/sections/map/MapRenderer.tsx
+++ src/sections/map/MapRenderer.tsx
@@ -3,91 +3,148 @@ import { Circle } from "./Circle";
 import { useRef } from "react";
 import { useDimensions } from "@/lib/use-dimensions";
 import { scaleOrdinal } from "d3";
-import { alertScale, evolutionScale, levelScale } from "./utils";
-import { ChangeLevelPill } from "./ChangeLevelPill";
+import { alertScale, levelScale } from "./utils";
+// import { ChangeLevelPill } from "./ChangeLevelPill";
 
 type MapProps = {
   data: MapObservation[];
 };
 
-//
-// Position of the bubbles
-//
-const xScale = scaleOrdinal<string, number>()
-  .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
-  .range([0.61, 0.598, 0.553, 0.519, 0.273]);
+// We are using iframes
+// So we need to deal with a FIXED HEIGHT for the map, whatever the screen size.
+// Se we decided to use 3 different map SVGs, and will use fixed dimensions for them.
+const HEIGHT = 700;
+const BREAK_POINTS_BIG = 1300;
+const BREAK_POINTS_SMALL = 500;
 
-const yScale = scaleOrdinal<string, number>()
-  .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
-  .range([0.51, 0.69, 0.59, 0.68, 0.62]);
+const SMALL_MAP_ASPECT_RATIO = 498.28 / 1080;
+const MEDIUM_MAP_ASPECT_RATIO = 754.37 / 1080;
+const BIG_MAP_ASPECT_RATIO = 1920 / 1080;
+
+const MAP_WIDTH_SMALL = SMALL_MAP_ASPECT_RATIO * HEIGHT;
+const MAP_WIDTH_MEDIUM = MEDIUM_MAP_ASPECT_RATIO * HEIGHT;
+const MAP_WIDTH_BIG = BIG_MAP_ASPECT_RATIO * HEIGHT;
+
+// Bubble positions are slightly different for each map background
+const LOCATIONS = [
+  "Carbonifère",
+  "Autres craie",
+  "Emmerin",
+  "Ansereuilles",
+  "Lys",
+];
+const POSITIONS_X = {
+  small: [298, 282, 246, 218, 29],
+  medium: [453, 427, 371, 330, 34],
+  big: [776, 765, 700, 649, 284],
+};
+const POSITIONS_Y = {
+  small: [344, 416, 366, 416, 377],
+  medium: [340, 447, 379, 446, 392],
+  big: [356, 480, 418, 477, 429],
+};
 
 //
 // Position of the little pills on top of each label
 //
-const xScaleEvolution = scaleOrdinal<string, number>()
-  .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
-  .range([0.715, 0.675, 0.453, 0.514, 0.155]);
+// const xScaleEvolution = scaleOrdinal<string, number>()
+//   .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
+//   .range([0.715, 0.675, 0.453, 0.514, 0.155]);
 
-const yScaleEvolution = scaleOrdinal<string, number>()
-  .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
-  .range([0.461, 0.735, 0.46, 0.753, 0.564]);
+// const yScaleEvolution = scaleOrdinal<string, number>()
+//   .domain(["Carbonifère", "Autres craie", "Emmerin", "Ansereuilles", "Lys"])
+//   .range([0.461, 0.735, 0.46, 0.753, 0.564]);
 
 //
 // Component
 //
 export const MapRenderer = ({ data }: MapProps) => {
-  const chartRef = useRef<HTMLDivElement>(null);
-  const chartSize = useDimensions(chartRef);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const containerSize = useDimensions(containerRef); // Used to know what map to display
 
-  // IMPORTANT
-  // We're working with iframes. So we have to provide a fixed height that must be
-  // respected for all screen sizes. It's a struggle to work with.
-  const height = 700;
+  const screenSize =
+    containerSize.width > BREAK_POINTS_BIG
+      ? "big"
+      : containerSize.width < BREAK_POINTS_SMALL
+      ? "small"
+      : "medium";
+
+  const mapWidth =
+    containerSize.width > BREAK_POINTS_BIG
+      ? MAP_WIDTH_BIG
+      : containerSize.width < BREAK_POINTS_SMALL
+      ? MAP_WIDTH_SMALL
+      : MAP_WIDTH_MEDIUM;
+
+  const circleRadius =
+    containerSize.width > BREAK_POINTS_BIG
+      ? 30
+      : containerSize.width < BREAK_POINTS_SMALL
+      ? 10
+      : 20;
+
+  const xScale = scaleOrdinal<string, number>()
+    .domain(LOCATIONS)
+    .range(POSITIONS_X[screenSize]);
+
+  const yScale = scaleOrdinal<string, number>()
+    .domain(LOCATIONS)
+    .range(POSITIONS_Y[screenSize]);
 
   return (
-    <div ref={chartRef} className="relative w-full" style={{ height }}>
-      <picture className="absolute inset-0 w-full h-full">
-        <source srcSet="map-bg-lg.svg" media="(min-width: 1300px)" />
-        <source srcSet="map-bg-md.svg" media="(min-width: 500px)" />
-        <img
-          src="map-bg-sm.svg"
-          className="block w-full h-full object-contain"
-          alt="Map background"
-        />
-      </picture>
+    <div
+      ref={containerRef}
+      className="relative w-full flex justify-center"
+      style={{ height: HEIGHT }}
+    >
+      <div className="relative">
+        <picture className="h-full">
+          <source srcSet="map-bg-lg.svg" media="(min-width: 1300px)" />
+          <source srcSet="map-bg-md.svg" media="(min-width: 500px)" />
+          <img src="map-bg-sm.svg" className="h-full" alt="Map background" />
+        </picture>
 
-      <svg
-        className="absolute inset-0"
-        style={{ width: chartSize.width, height }}
-      >
-        {data.map((d, i) => {
-          return (
+        <svg
+          className="absolute inset-0"
+          style={{ width: mapWidth, height: HEIGHT }}
+          // onClick={(e) => {
+          //   const svg = e.currentTarget;
+          //   const point = svg.createSVGPoint();
+          //   point.x = e.clientX;
+          //   point.y = e.clientY;
+          //   const svgPoint = point.matrixTransform(
+          //     svg.getScreenCTM().inverse()
+          //   );
+          //   console.log(`Clicked at: x=${svgPoint.x}, y=${svgPoint.y}`);
+          // }}
+        >
+          {data.map((d, i) => (
             <Circle
               key={i}
-              cx={xScale(d.CHAMP_CAPTANT) * chartSize.width}
-              cy={yScale(d.CHAMP_CAPTANT) * height}
-              r={35}
+              cx={xScale(d.CHAMP_CAPTANT)}
+              cy={yScale(d.CHAMP_CAPTANT)}
+              r={circleRadius}
               level={levelScale(d.CARACT)}
               alert={alertScale(d.SEUIL)}
             />
-          );
-        })}
-      </svg>
+          ))}
+        </svg>
+      </div>
 
-      {data.map((d, i) => {
+      {/* {data.map((d, i) => {
         return (
           <div
             key={i}
             className="absolute inset-0"
             style={{
-              left: xScaleEvolution(d.CHAMP_CAPTANT) * chartSize.width,
-              top: yScaleEvolution(d.CHAMP_CAPTANT) * height,
+              left: xScaleEvolution(d.CHAMP_CAPTANT) * containerSize.width,
+              top: yScaleEvolution(d.CHAMP_CAPTANT) * HEIGHT,
             }}
           >
             <ChangeLevelPill levelName={evolutionScale(d.EVOLUTION)} />
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
