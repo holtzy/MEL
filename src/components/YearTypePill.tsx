7851c196baf92a9a92b9c8e67274303a5e52eca8
Holtz Yan <yan.holtz@datadoghq.com>
2025-04-08 09:20:22 +0200

    some mobile improvement

--- src/components/YearTypePill.tsx
+++ src/components/YearTypePill.tsx
@@ -1,6 +1,6 @@
 export const YearTypePill = ({ yearType }: { yearType: string }) => {
   return (
-    <span className="flex items-center h-7 px-4 py-2 rounded-2xl border border-black text-sm">
+    <span className="flex items-center h-7 px-4 py-2 rounded-2xl border border-black text-sm text-nowrap">
       {"Année " + yearType.toLowerCase()}
     </span>
   );
