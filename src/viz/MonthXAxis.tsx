11964dfa5e4cd42799129723549eefe181b76050
Holtz Yan <yan.holtz@datadoghq.com>
2025-04-08 09:16:24 +0200

    remove some labels on small scrren

--- src/viz/MonthXAxis.tsx
+++ src/viz/MonthXAxis.tsx
@@ -19,21 +19,32 @@ export const MonthXAxis = ({ xScale, y }: MonthXAxisProps) => {
         >
           {bandValue}
         </text>
-        {/* <line
-          y1={boundsHeight + 10}
-          y2={boundsHeight + 26}
-          x1={
-            x + xScale.bandwidth() + (xScale.padding() * xScale.bandwidth()) / 2
-          }
-          x2={
-            x + xScale.bandwidth() + (xScale.padding() * xScale.bandwidth()) / 2
-          }
-          stroke="#212121"
-          strokeWidth={0.2}
-        /> */}
       </g>
     );
   });
 
-  return <g>{xLabels}</g>;
+  const xLabelsSmallScreen = xScale.domain().map((bandValue, i) => {
+    if (i % 2 !== 0) return null;
+    return (
+      <g key={i}>
+        <text
+          x={(xScale(bandValue) ?? 0) + xScale.bandwidth() / 2}
+          y={y}
+          textAnchor="middle"
+          alignmentBaseline="central"
+          fontSize={12}
+          fill="black"
+        >
+          {bandValue}
+        </text>
+      </g>
+    );
+  });
+
+  return (
+    <>
+      <g className="hidden sm:block">{xLabels}</g>
+      <g className="block sm:hidden">{xLabelsSmallScreen}</g>
+    </>
+  );
 };
